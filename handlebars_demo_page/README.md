b3c31d87db04f5583ba25490f2009d5c91a95c35
Ken Hawkins <khawkins98@gmail.com>
2014-08-30 10:00:00 +0200

    Adds "Handlebars Demo Page" sample module

---
+++ handlebars_demo_page/README.md
@@ -0,0 +1,14 @@
+Handlebars Demo Page
+====================
+
+Simple implementation of a module that adds a page and registers a Handlebars template and JS file.
+
+Instructions
+------------
+
+1. Add this to your site's modules directory.
+2. Add the [handlebars.js file](http://handlebarsjs.com/) to your libraries/handlebars directory (i.e. /sites/all/libraries/handlebars/handlebars-v1.3.0.js )
+1. Follow the Handlebars module instructions.
+2. Enable this module.
+3. Go to (yoursite.com)/handlebars/demo/page
+4. Check out the code.
---
+++ handlebars_demo_page/handlebars_demo_page.js
@@ -0,0 +1,62 @@
+(function ($) {
+
+    $(document).ready(function(){
+
+        /**
+         *  Reference the template.
+         */
+
+        var source = $("#handlebars_demo_page").html();
+        var template = Handlebars.compile(source); 
+
+        /**
+         *  Extend functionality.
+         */
+
+        // Our JSON gives just the image URL, make it wraped in an img tag and with the media style.
+        // Syntax: {{{imageRendererd image}}}
+
+        Handlebars.registerHelper('imageRendererd', function(image) {
+
+            // change: /sites/default/files/image.jpg
+            // to:     /sites/default/files/styles/large/public/image.jpg   
+
+            image = image.replace(/\/files\//, "\/files\/styles\/large\/public\/");        
+
+            return "<img src='" + image + "'>'";
+        });
+
+        /**
+         *  Load our data and process it into the document.
+         */
+
+        // If you wish to load in data remotely. Perhaps from the Service Views module?
+
+        // $.get("/rest/views/rest_gallery/",function(data,status,xhr) {
+        //     // Wrap the retireved data in a parent object.
+        //     var dataNodes = new Object();
+        //     dataNodes.nodes = data;
+        //     $('.handlebars-content-target').append(template(dataNodes));
+        // });
+
+        // --------------------------
+
+        // Or, load fixed data.
+
+        var data = { 
+            nodes: 
+            [
+                {"title":"Grab n drag","body":"<p>Images are linked so you can click to explore further.</p>\n","image":"http://work.allaboutken.com/sites/work.allaboutken.com/files/grab-drag-animation_0.gif","nid":"12","link":null},
+                {"title":"Stuttgart Tweed Run 2014","body":"<p>Raising euros for charity, the annual Stuttgart Tweed Run offers donors a great old-fashioned time. The website conveys that experience through large visuals with the ability to read, watch, and sign-up on the same page.</p>\n","image":"http://work.allaboutken.com/sites/work.allaboutken.com/files/tweed-2014-2.jpg","nid":"19","link":"http://tweedrun.stuttgart-society.org/"}
+            ]    
+        }; 
+
+        // Be sure to only use this if you load data directly from this JS file.
+
+        $('.handlebars-content-target').append(template(data));
+
+        // --------------------------
+
+    });
+
+}(jQuery));
